c4c9bf95cf03e99da03149e736c85ebd38ad5477
Viktoriiiaa <115409499+Viktoriiiaa@users.noreply.github.com>
2022-12-08 21:33:12 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,131 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами
+/*
+
+Console.WriteLine("Введите количество строк  ");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[linesVol, columnsVol];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+*/
+
+// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет
+/*
+
+Console.Write("Введите строку: ");
+int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Введите столбец: ");
+int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
+int n = 5; 
+int m = 7; 
+Random random = new Random();
+int[,] arr = new int[n, m];
+Console.WriteLine("Исходный массив: ");
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+{
+    arr[i, j] = random.Next(10, 99);
+Console.Write("{0} ", arr[i, j]);
+}
+Console.WriteLine();
+}
+    if (pos1 < 0 | pos1 > arr.GetLength(0) - 1 | pos2 < 0 | pos2 > arr.GetLength(1) - 1)
+{
+Console.WriteLine("Элемент не существует  ");
+}
+    else
+{
+    Console.WriteLine("Значение элемента массива = {0}", arr[pos1, pos2]);
+}
+Console.ReadLine();
+*/
+
+// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце
+
+/*
+int m = 3;
+int n = 4;
+int[,] array = new int[m, n];
+
+FillArray(array);
+PrintArray(array);
+PrintAvgArray(Average(array));
+
+void FillArray(int[,] arr)
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+
+
+        }
+    }
+}
+void PrintArray(int[,] ar)
+{
+    for (int i = 0; i < ar.GetLength(0); i++)
+    {
+        for (int j = 0; j < ar.GetLength(1); j++)
+        {
+            Console.Write($"{ar[i, j],1} ");
+
+        }
+        Console.WriteLine("");
+    }
+}
+double[] Average(int[,] ar)
+{
+    double[] avg = new double[ar.GetLength(1)];
+    for (int i = 0; i < ar.GetLength(0); i++)
+    {
+        for (int j = 0; j < ar.GetLength(1); j++)
+        {
+            avg[j] += Convert.ToDouble(ar[i, j]);
+        }
+    }
+    for (int k = 0; k < avg.Length; k++)
+    {
+        avg[k] /= ar.GetLength(0);
+       
+    }
+    return avg;
+}
+
+void PrintAvgArray(double[] avgArray)
+{
+    for (int i = 0; i < avgArray.Length; i++)
+    {
+        Console.WriteLine($"Среднеарифмитическое столбца {i + 1} равно {avgArray[i]}");
+    }
+}
+*/ 
